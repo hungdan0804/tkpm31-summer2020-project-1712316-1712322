6e5b2927d024f883c4651483e525dadd32dd6b39
316324 <hungdan123456@gmail.com>
2020-05-25 10:43:10 +0700

    [Initialize Project]

---
+++ app/src/main/java/com/hcmus/tkpm31_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hcmus.tkpm31_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
